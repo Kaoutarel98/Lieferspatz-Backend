cb2aaedb04ba21e131a336b2a6ca15b7b4dabc24
Rahaf Tabbaa <rahaf27ta@gmail.com>
2024-12-11 09:13:12 +0100

    init java

---
+++ src/main/java/com/uni/lieferspatz/LieferspatzApplication.java
@@ -0,0 +1,13 @@
+package com.uni.lieferspatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LieferspatzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LieferspatzApplication.class, args);
+	}
+
+}
