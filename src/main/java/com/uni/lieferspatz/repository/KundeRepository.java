f16d39cfbbd40a3283c78b44a4d5c20fd4c4861a
Rahaf Tabbaa <rahaf27ta@gmail.com>
2025-01-26 20:00:52 +0000

    Add comments

--- src/main/java/com/uni/lieferspatz/repository/KundeRepository.java
+++ src/main/java/com/uni/lieferspatz/repository/KundeRepository.java
@@ -9,8 +9,9 @@ import com.uni.lieferspatz.domain.Kunde;
 
 @Repository
 public interface KundeRepository extends JpaRepository<Kunde, Long> {
-
+    // SELECT * FROM kunde WHERE email = ?
     public Optional<Kunde> findByEmail(String email);
 
+    // SELECT * FROM kunde WHERE UPPER(email) = UPPER(?)
     public Optional<Kunde> findOneByEmailIgnoreCase(String email);
 }
